9da0f05eea6c01fbfd80ba180778b9e2369a1680
nminh123 <160375525+nminh123@users.noreply.github.com>
2025-06-19 01:06:05 +0700

    draw X & O

--- include/Constants.hpp
+++ include/Constants.hpp
@@ -2,7 +2,10 @@
 
 namespace Constants
 {
-    constexpr unsigned int SCREEN_WIDTH         =   800;
-    constexpr unsigned int SCREEN_HEIGHT        =   600;
+    constexpr unsigned int WINDOW_WIDTH         =   1250;
+    constexpr unsigned int WINDOW_HEIGHT        =   800;
     constexpr unsigned int FRAME_PER_SECOND     =   60;
+    constexpr float O_POINTCOUNT                =   60.f;
+    constexpr float O_RADIUS                    =   100.f;
+    constexpr sf::Color O_COLOR                     (0, 170, 255);
 }
--- src/game.cpp
+++ src/game.cpp
@@ -5,8 +5,12 @@
 int main(int, char**)
 {
     // sf::Window window(sf::VideoMode(sf::Vector2u(Constants::SCREEN_WIDTH, Constants::SCREEN_HEIGHT), "Tic tac toe");
-    sf::RenderWindow window(sf::VideoMode({Constants::SCREEN_WIDTH, Constants::SCREEN_HEIGHT}), "Tic tac toe", sf::Style::Default);
+    sf::RenderWindow window(sf::VideoMode({Constants::WINDOW_WIDTH, Constants::WINDOW_HEIGHT}), "Tic tac toe", sf::Style::Default);
     window.setFramerateLimit(Constants::FRAME_PER_SECOND);
+
+    // sf::CircleShape O;
+    // O.setPointCount(Constants::O_POINTCOUNT);
+    sf::RectangleShape X;
 
     while(window.isOpen())
     {
@@ -17,7 +21,27 @@ int main(int, char**)
                 window.close();
             }
         }
+
+        float window_min = (Constants::WINDOW_WIDTH < Constants::WINDOW_HEIGHT) ? Constants::WINDOW_WIDTH : Constants::WINDOW_HEIGHT;
         window.clear();
+        // X Symbol
+        X.setSize({200.f, 20.f});
+        X.setOrigin(X.getGeometricCenter());
+        X.setFillColor(sf::Color::Green);
+        X.setPosition({Constants::WINDOW_WIDTH / 2.f, Constants::WINDOW_HEIGHT / 2.f});
+        X.setRotation(sf::degrees(45));
+        window.draw(X);
+        X.setRotation(sf::degrees(-45));
+        window.draw(X);
+
+        // O Symbol
+        // O.setRadius(Constants::O_RADIUS);
+        // O.setOrigin(O.getGeometricCenter());
+        // O.setFillColor(sf::Color::Blue);
+        // O.setOutlineColor(Constants::O_COLOR);
+        // O.setOutlineThickness(-5.f);
+        // O.setPosition({Constants::WINDOW_WIDTH / 2.f, Constants::WINDOW_HEIGHT / 2.f});
+        // window.draw(O);
         window.display();
     }
     return 0;
