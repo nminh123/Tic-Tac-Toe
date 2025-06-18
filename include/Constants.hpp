f01f36c64c86aa3f15e8406fe101fe782d731807
nminh123 <160375525+nminh123@users.noreply.github.com>
2025-06-19 00:31:45 +0700

    setup window

---
+++ include/Constants.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace Constants
+{
+    constexpr unsigned int SCREEN_WIDTH         =   800;
+    constexpr unsigned int SCREEN_HEIGHT        =   600;
+    constexpr unsigned int FRAME_PER_SECOND     =   60;
+}
--- src/game.cpp
+++ src/game.cpp
@@ -1,5 +1,24 @@
+#include <SFML/Graphics.hpp>
 #include <iostream>
+#include "../include/Constants.hpp"
 
-int main(int, char**){
-    std::cout << "Hello, from Tic-Tac-Toe!\n";
+int main(int, char**)
+{
+    // sf::Window window(sf::VideoMode(sf::Vector2u(Constants::SCREEN_WIDTH, Constants::SCREEN_HEIGHT), "Tic tac toe");
+    sf::RenderWindow window(sf::VideoMode({Constants::SCREEN_WIDTH, Constants::SCREEN_HEIGHT}), "Tic tac toe", sf::Style::Default);
+    window.setFramerateLimit(Constants::FRAME_PER_SECOND);
+
+    while(window.isOpen())
+    {
+        while(const std::optional event = window.pollEvent())
+        {
+            if(event->is<sf::Event::Closed>() || sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Space))
+            {
+                window.close();
+            }
+        }
+        window.clear();
+        window.display();
+    }
+    return 0;
 }
